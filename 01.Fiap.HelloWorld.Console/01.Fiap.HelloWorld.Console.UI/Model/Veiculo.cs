5cd9fa3b57c17630bb3371ba92e63617fb071cb4
Logon Aluno <logonrmlocal@fiap.com.br>
2018-08-10 20:29:19 -0300

    Aula 01

---
+++ 01.Fiap.HelloWorld.Console/01.Fiap.HelloWorld.Console.UI/Model/Veiculo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Fiap.HelloWorld.Console.UI.Model
+{
+    class Veiculo
+    {
+        private double _potencia;
+
+        public int Ano { get; set; }
+        public double Potencia
+        {
+            get { return _potencia; }
+            set
+            {
+                if(value >=0)
+                {
+                    _potencia = value;
+                }
+            }
+        }
+    }
+}
---
+++ 01.Fiap.HelloWorld.Console/01.Fiap.HelloWorld.Console.UI/Program.cs
@@ -0,0 +1,27 @@
+﻿using _01.Fiap.HelloWorld.Console.UI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Fiap.HelloWorld.Console.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro carro = new Carro();
+            carro.Ano = 2010;
+            carro.ArCondicionado = false;
+
+            System.Console.WriteLine(carro.Ano);
+
+            var lancha = new Lancha()
+            {
+                BoiaSalvaVida = true,
+                Ano = 1999
+            };
+        }
+    }
+}
